9510155a26c3344dd0a5777a36e31439402e7a8a
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-04 22:04:45 +0100

    Make palindrome exercise

--- src/main/java/daw/Palindrome.java
+++ src/main/java/daw/Palindrome.java
@@ -10,4 +10,40 @@ package daw;
  */
 public class Palindrome {
 
+    public static void main(String[] args) {
+        String[] words = {
+            "level", // Palindrome
+            "world", // Not a palindrome
+            "radar", // Palindrome
+            "java", // Not a palindrome
+            "civic", // Palindrome
+            "hello", // Not a palindrome
+            "madam", // Palindrome
+            "openai", // Not a palindrome
+            "racecar", // Palindrome
+            "example" // Not a palindrome
+        };
+
+        for (String word : words) {
+            System.out.println(word + " is palindrome -> "
+                    + isPalindrome(word.toLowerCase().toCharArray()));
+        }
+    }
+    
+    /**
+     * 
+     * @param charArr an array of characters to be checked
+     * @return  {@code true} if the array is a palindrome, 
+     *          {@code false} otherwise.
+     */
+    public static boolean isPalindrome(char[] charArr) {
+        // Starts on left and compares to the letter on right, both going
+        // to the center
+        for (int i = 0; i <= charArr.length / 2; i++) {
+            if (charArr[i] != charArr[(charArr.length - 1) - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
