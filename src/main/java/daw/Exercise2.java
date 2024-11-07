cbe8ac407160c7802f580d9fc1a16912c7243ea5
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-07 09:58:19 +0100

    finish exercise2 class

--- src/main/java/daw/Exercise2.java
+++ src/main/java/daw/Exercise2.java
@@ -4,10 +4,53 @@
  */
 package daw;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author alvaro
  */
 public class Exercise2 {
-    
+
+    public static void main(String[] args) {
+        final int TAM = 10;
+
+        ArrayList<Integer> list1 = new ArrayList<>(TAM);
+        ArrayList<Integer> list2 = new ArrayList<>(TAM);
+        ArrayList<Integer> listRes = new ArrayList<>(TAM);
+
+        giveRandomValues(list1, TAM);
+        giveRandomValues(list2, TAM);
+        
+        showList(list1);
+        showList(list2);
+        
+        listRes = multiplyLists(list1, list2);
+        showList(listRes);
+    }
+
+    public static void giveRandomValues(ArrayList<Integer> list, int tam) {
+        Random rd = new Random();
+
+        for (int i = 0; i < tam; i++) {
+            int n = rd.nextInt(1, 100) + 1;
+            list.add(n);
+        }
+    }
+
+    public static void showList(ArrayList<Integer> list) {
+        System.out.println(list.toString());
+    }
+
+    public static ArrayList<Integer> multiplyLists(ArrayList<Integer> listA, 
+            ArrayList<Integer> listB) {
+        ArrayList<Integer> listRes = new ArrayList<>();
+        
+        for (int i = 0; i < listA.size(); i++) {
+            listRes.add(listA.get(i) * listB.get(i));
+        }
+        
+        return listRes;
+    }
 }
