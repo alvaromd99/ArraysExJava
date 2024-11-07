a4f367b9826c1547cfd6c0fe86f1b3f49933cbd8
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-07 10:36:58 +0100

    finish exercise3

--- src/main/java/daw/Exercise3.java
+++ src/main/java/daw/Exercise3.java
@@ -4,13 +4,35 @@
  */
 package daw;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author alvaro
  */
 public class Exercise3 {
-    
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = 0;
+
+        do {
+            System.out.println("Introduce un numero entre 1 y 20. Introduce"
+                    + " 0 para salir:");
+            n = sc.nextInt();
+            list.add(n);
+        } while (n != 0);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            System.out.print((i + 1) + ": ");
+
+            for (int j = 0; j < list.get(i); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
