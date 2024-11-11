d72a518c12207ad295c0fef7ad85f8c3775dc654
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-11 22:06:02 +0100

    add javadoc to all the methods

--- src/main/java/daw/Ejercicio4.java
+++ src/main/java/daw/Ejercicio4.java
@@ -29,13 +29,13 @@ public class Ejercicio4 {
 
         System.out.println("Lista de los pins en la lista principal: ");
         showListOfArray(list);
-        
+
         // Parte 1
         num = getNumber();
         pinList = getPinWithNum(list, num);
         System.out.println("Lista de los pins con el numero " + num + ": ");
         showListOfArray(pinList);
-        
+
         // Parte 2
         arr = getIntArray();
         System.out.println("Array del usuario: ");
@@ -44,12 +44,24 @@ public class Ejercicio4 {
                 + isArrayInList(list, arr));
     }
 
+    /**
+     * Muestra por consola los arrays que contiene la lista.
+     *
+     * @param list la lista de arrays que se va a mostrar.
+     */
     public static void showListOfArray(ArrayList<int[]> list) {
         for (int[] el : list) {
             System.out.println(Arrays.toString(el));
         }
     }
 
+    /**
+     * Crea una lista de arrays de tamaño 4, con números aleatorios en cada
+     * array. Los números generados no se repiten dentro de cada array.
+     *
+     * @param tam el número de arrays que tendrá la lista.
+     * @return la lista llena con arrays de números aleatorios sin repeticiones.
+     */
     public static ArrayList<int[]> randomPinList(int tam) {
         ArrayList<int[]> list = new ArrayList<>();
 
@@ -68,6 +80,12 @@ public class Ejercicio4 {
         return list;
     }
 
+    /**
+     * Genera un array de 4 enteros con números aleatorios entre 0 y 9 sin
+     * repeticiones.
+     *
+     * @return un array de 4 números aleatorios sin repeticiones.
+     */
     public static int[] generatePinNoRepetition() {
         Random rd = new Random();
 
@@ -85,6 +103,14 @@ public class Ejercicio4 {
         return arr;
     }
 
+    /**
+     * Comprueba si un número se encuentra dentro de un array.
+     *
+     * @param arr el array en el que buscamos el número.
+     * @param n el número que buscamos en el array.
+     * @return {@code true} si el número se encuentra en el array, {@code false}
+     * si el número no está en el array.
+     */
     public static boolean isInArray(int[] arr, int n) {
         for (int el : arr) {
             if (el == n) {
@@ -94,6 +120,14 @@ public class Ejercicio4 {
         return false;
     }
 
+    /**
+     * Obtiene una lista de los arrays en la lista original que contienen el
+     * número especificado.
+     *
+     * @param list la lista de arrays donde se realizará la búsqueda.
+     * @param n el número que se busca dentro de los arrays.
+     * @return una lista de arrays que contienen el número especificado.
+     */
     public static ArrayList<int[]> getPinWithNum(ArrayList<int[]> list, int n) {
         ArrayList<int[]> listRes = new ArrayList<>();
 
@@ -105,6 +139,11 @@ public class Ejercicio4 {
         return listRes;
     }
 
+    /**
+     * Pedimos un numero entero al usuario, contralando la excepcion
+     *
+     * @return el numero que ha introducido el usuario
+     */
     public static int getNumber() {
         Scanner sc = new Scanner(System.in);
         int n = 0;
@@ -122,6 +161,13 @@ public class Ejercicio4 {
         return n;
     }
 
+    /**
+     * Solicita al usuario que introduzca un array de 4 números enteros,
+     * asegurándose de que no haya repeticiones.
+     *
+     * @return un array de 4 números introducidos por el usuario sin
+     * repeticiones.
+     */
     public static int[] getIntArray() {
         int[] arr = new int[4];
         int n = 0;
@@ -140,6 +186,15 @@ public class Ejercicio4 {
         return arr;
     }
 
+    /**
+     * Compara un array con los arrays contenidos en una lista y verifica si el
+     * array introducido es igual a alguno de los de la lista.
+     *
+     * @param list la lista de arrays en la que se buscará el array.
+     * @param arr el array que se va a buscar en la lista.
+     * @return  {@code true}  si el array se encuentra en la lista
+     *          {@code false} si no se encuentra en la lista.
+     */
     public static boolean isArrayInList(ArrayList<int[]> list, int[] arr) {
         for (int[] listElem : list) {
             if (Arrays.equals(listElem, arr)) {
